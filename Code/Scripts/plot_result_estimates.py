8f53492e6c8465e7e00fe2ed772faf4602c9e440
Paavo Hietala <paavo.hietala@aalto.fi>
2022-10-31 16:16:14 +0200

    Updated source estimate plot filetype, dpi & docs

--- Code/Scripts/plot_result_estimates.py
+++ Code/Scripts/plot_result_estimates.py
@@ -3,7 +3,7 @@
 """
 Created on Wed Jun 16 16:31:07 2021
 
-Plot source estimates for section 4.1. of the Thesis.
+Plot source estimates for Figure 4.
 
 Adapted from https://mne.tools/stable/auto_examples/visualization/publication_figure.html
 
@@ -19,68 +19,53 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import (make_axes_locatable, ImageGrid,
                                      inset_locator)
 import sys
+import mne
+
 sys.path.append(op.dirname(op.dirname(op.realpath(__file__))))
 from Core import utils
 
-import mne
-
-#
 # Script settings --------------------------------------------------------------
 #
-# Produces all 18 figures for thesis section 4.1. Source estimates.
-# First, 6 eLORETA images are plotted followed by 6 reMTW & AVG and 6 plain
-# reMTW plots.
+# Produces all 9 source estimates for Figure 4: Source estimates.
+# First, 3 eLORETA images are plotted followed by 3 reMTW and 3 reMTW & AVG plots.
 #
 # Plots are saved to <project_dir>/Data/plot/<filename of stc>
-#
 
-# Root data directory of the project, str
+# Root data directory for each source estimate, list of str
 
-#project_dirs = ['/m/nbe/scratch/megci/MFinverse/Classic/'] * 6 \
-#               + ['/m/nbe/scratch/megci/MFinverse/reMTW/'] * 12
-project_dirs = ['/m/nbe/scratch/megci/MFinverse/reMTW/'] * 6
+project_dirs = ['/m/nbe/scratch/megci/MFinverse/Classic/'] * 3 \
+               + ['/m/nbe/scratch/megci/MFinverse/reMTW/'] * 6
 
-# Subjects' MRI location, str
+# Subjects' MRI location (FreeSurfer dir), str
 
 subjects_dir = '/m/nbe/scratch/megci/data/FS_Subjects_MEGCI/'
-mne.set_config('SUBJECTS_DIR', subjects_dir)
 
 # List of subjects, indices within all following lists must match
 
-#subjects = ['MEGCI_S1', 'fsaverage', 'fsaverage'] * 2 \
-#           + ['fsaverage', 'fsaverage', 'fsaverage'] * 2 \
-#           + ['MEGCI_S1'] * 6
-subjects = ['fsaverage'] * 3 + ['MEGCI_S1'] * 3
+subjects = ['MEGCI_S1', 'fsaverage', 'fsaverage'] + ['MEGCI_S1'] * 3 \
+            + ['fsaverage'] * 3
 
 # List of stimuli
 
-#stims = (['sector16'] * 3 + ['sector15'] * 3) * 4
-stims = ['sector16'] * 6
+stims = ['sector16'] * 9
 
-# List of methods used for each stc
+# List of inverse solution methods used for each stc
 
-#methods = ['eLORETA'] * 6 + ['remtw'] * 12
-methods = ['remtw'] * 6
+methods = ['eLORETA'] * 3 + ['remtw'] * 6
 
 # Suffixes of stcs, can be None for indiviudal stcs
 
-#suffixes = [None, '10subjects', '20subjects'] * 2 \
-#           + ['1subjects', '10subjects', '20subjects'] * 4
-suffixes = ['1subjects', '10subjects', '20subjects'] * 2
+suffixes = [None, '10subjects', '20subjects'] \
+           + ['1subjects', '10subjects', '20subjects'] * 2
 
-# Types of each stc. stc, stc_m or avg
+# Types of each stc. stc (individual), stc_m (individual on fsaverage mesh) or
+# avg (averaged data on fsaverage mesh)
 
-#stc_types = ['stc_m', 'avg', 'avg'] * 2 \
-#            + ['avg', 'avg', 'avg'] * 2 \
-#            + ['stc_m'] * 6
-stc_types = ['avg'] * 3 + ['stc_m'] * 3
-
+stc_types = ['stc_m', 'avg', 'avg'] + ['stc_m'] * 3 + ['avg'] * 3
+            
 # Target source space of stc_m and avg, usually fsaverage
 
-#src_tos = ['fsaverage', None, None] * 2 \
-#          + [None, None, None] * 2 \
-#          + ['fsaverage'] * 6
-src_tos = [None, None, None, 'fsaverage', 'fsaverage', 'fsaverage']
+src_tos = ['fsaverage', None, None] + ['fsaverage'] * 3 + [None, None, None]
 
 # Get peak for selected time only
 
@@ -89,30 +74,29 @@ time = 0.081
 # Plot colorbar from 0 to max (abs) or from -max to +max (bi), abs to separate
 # file (sep), bi to separate file (bisep) or None
 
-#cbars = [None] * 5 + ['sep'] + ['abs', 'abs', 'abs'] * 2 + ['bi'] * 6
-cbars = ['sep'] * 3 + ['bisep'] * 3
+cbars = [None, None, 'sep'] + ['bisep'] * 3 + ['sep'] * 3
 
 # Overwrite existing files
 
 overwrite = True
 
 # Custom clims, mainly for eLORETA to set all 6 to same limits
-'''
+
 abs_max = 0
-for stim, suffix in zip(stims[:6], suffixes[:6]):
+for stim, suffix in zip(stims[:3], suffixes[:3]):
     stc = mne.read_source_estimate(project_dirs[0]
                                    + 'Data/avg/fsaverage-ico4-eLORETA-f-'
                                    + stim + ('-' + suffix if suffix else ''))
     if np.max(abs(stc.data)) > abs_max:
-        abs_max = np.max(abs(stc.data)) * 0.75
+        abs_max = np.max(abs(stc.data))
 
-clims = [{'kind' : 'value', 'lims' : [0, 0.5 * abs_max, abs_max]}] * 6 + [None] * 12
-'''
-clims = [None] * 6
+clims = [{'kind' : 'value', 'lims' : [0, 0.5 * abs_max, abs_max]}] * 3 + [None] * 6
 
 #
 # Run the script for all data --------------------------------------------------
 #
+
+mne.set_config('SUBJECTS_DIR', subjects_dir)
 
 def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
                           stc_type = 'stc', src_to = 'fsaverage',
@@ -147,6 +131,7 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
     clim : dict, optional
         Clims for the color bar, by default None for automatic clims
     '''
+
     # Load source estimate
     fname_stc = '-'.join(filter(None, [subject, 'ico4', method, src_to, 'f',
                                        stim, suffix, 'lh.stc']))
@@ -168,7 +153,7 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
         if np.count_nonzero(stc.rh_data) == 0:
             stc.rh_data[0] = 1e-20
         
-    # Plot the STC with V1 borders, get the brain image, crop it:
+    # Plot the STC with V1 borders:
     brain = stc.plot(hemi='split', size=(1500,600),
                      subject = ( src_to if stc_type == 'stc_m' else subject),
                      initial_time = (time if stc.data.shape[1] > 1 else None),
@@ -202,12 +187,14 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
         elif cbar_type in ['bi', 'bisep']:
             clim = dict(kind='value', pos_lims=[0, 0.5 * maxi, maxi])
     
+    # Add V1 border
     for hemi in ['lh', 'rh']:
         v1 = mne.read_label('/m/nbe/scratch/megci/data/FS_Subjects_MEGCI/'
                             + 'fsaverage/label/' + hemi + '.V1_exvivo.label',
                             'fsaverage')
         brain.add_label(v1, borders = 2, color = 'lime')
 
+    # Crop whitespace around the brain
     brain.show_view({'elevation' : 100, 'azimuth' : -60}, distance = 400, col = 0)
     brain.show_view({'elevation' : 100, 'azimuth' : -120}, distance = 400, col = 1)
     screenshot = brain.screenshot()
@@ -220,28 +207,26 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
 
     # Tweak the figure style
     plt.rcParams.update({
-        'ytick.labelsize': 'large',
-        'xtick.labelsize': 'large',
-        'axes.labelsize': 'large',
-        'axes.titlesize': 'large',
+        'font.size' : 22,
         'grid.color': '0.75',
         'grid.linestyle': ':',
     })
 
     # Create new fig with subplots to get axes easily
-    fig, axes = plt.subplots(num = fname_stc, figsize = (4, 4.))
+    fig, axes = plt.subplots(num = fname_stc, figsize = (10, 7.))
 
     # now add the brain to the axes
     axes.imshow(cropped_screenshot)
     axes.axis('off')
 
+    # Save source estimates if colorbar is rendered separately
     if cbar_type in [None, 'sep', 'bisep']:
         plt.savefig(fpath_out, bbox_inches = 'tight', pad_inches = 0.0)
         if cbar_type == None:
             return
         else:
             fpath_out = fpath_out[:-4] + '-colorbar.png'
-            fig, axes = plt.subplots(num = fname_stc + '-cb', figsize = (3, 4.))
+            fig, axes = plt.subplots(num = fname_stc + '-cb', figsize = (7, 7.))
             axes.axis('off')
 
     # add a horizontal colorbar with the same properties as the 3D one
@@ -255,10 +240,10 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
         base, exponent = str(maxi).split('e')
         base = str(round(float(base), 2))
         cbar = mne.viz.plot_brain_colorbar(cax, clim,
-                                            orientation = 'horizontal',
-                                            label = r'Activation (Am) '
-                                            + r'$\cdot 10^{'
-                                            + exponent + r'}$')
+                                           orientation = 'horizontal',
+                                           label = r'Activation (Am) '
+                                           + r'$\cdot 10^{'
+                                           + exponent + r'}$')
         if cbar_type in ['bi', 'bisep']:
             cbar.set_ticks([-maxi, 0, maxi])
             cbar.set_ticklabels(["%.2f" % -float(base), "%.2f" % 0.00,
@@ -267,18 +252,22 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
             cbar.set_ticks([0, maxi / 2, maxi])
             cbar.set_ticklabels(["%.2f" % 0.00, "%.2f" % (float(base) / 2),
                                  "%.2f" % float(base)])
+            
     # Split fails = not exponential. Use plain floats as ticks instead
     except ValueError:
         print('Splitting exponent failed')
         cbar = mne.viz.plot_brain_colorbar(cax, clim,
-                                            orientation = 'horizontal',
-                                            label = 'Activation (Am)')
+                                           orientation = 'horizontal',
+                                           label = 'Activation (Am)')
         if cbar_type in ['bi', 'bisep']:
             cbar.set_ticks([round(-maxi, 2), 0, round(maxi, 2)]) 
             cbar.set_ticklabels(["%.2f" % -maxi, "%.2f" % 0.00, "%.2f" % maxi])
         else:    
             cbar.set_ticks([0, round(maxi / 2, 2), round(maxi, 2)]) 
             cbar.set_ticklabels(["%.2f" % 0.00, "%.2f" % (maxi / 2), "%.2f" % maxi])
+    
+    cbar.outline.set_visible(True)
+
     # tweak margins and spacing
     fig.subplots_adjust(left = 0.15, right = 0.9, bottom = 0.01, top = 0.9,
                         wspace = 0.1, hspace = 0.5)
@@ -286,10 +275,10 @@ def plot_result_estimates(subject, stim, method, project_dir, suffix = None,
     # Save image        
     if cbar_type in ['sep', 'bisep']:
         plt.tight_layout()
+        
         # Save colorbar to buffer
-
         buf = BytesIO()
-        fig.savefig(buf)
+        fig.savefig(buf, dpi = 500)
         buf.seek(0)
         cb = Image.open(buf)
         cb.load()
